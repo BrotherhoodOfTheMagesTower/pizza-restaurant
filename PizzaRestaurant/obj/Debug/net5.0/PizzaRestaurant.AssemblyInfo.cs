a2be14a766b6b026f495dc3f610facdfe36ceaff
Dominik <depta.dominik@op.pl>
2022-01-09 22:53:41 +0100

    refactored solution to use register/login page and applied default docker file

--- PizzaRestaurant/obj/Debug/net5.0/PizzaRestaurant.AssemblyInfo.cs
+++ PizzaRestaurant/obj/Debug/net5.0/PizzaRestaurant.AssemblyInfo.cs
@@ -11,6 +11,8 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap4")]
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-PizzaRestaurant-9FF13190-1CB5-43E9-AC05-D17976AED761")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("PizzaRestaurant")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
