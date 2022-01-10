3e1c9a4def9c6bf00e8c9da7405d853325c5f860
Dom3r <kubsler123@gmail.com>
2022-01-10 13:07:04 +0100

    Added Models

---
+++ PizzaRestaurant/Models/Account.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaRestaurant.Models
+{
+    public class Account
+    {
+        public int AccountID { get; set; }
+        public string Password { get; set; }
+        public string Login { get; set; }
+        public DateTime CreationDate { get; set; }
+        public int PromotionPoints { get; set; }
+    }
+}
---
+++ PizzaRestaurant/Models/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PizzaRestaurant.Models
+{
+    public class Worker:Account
+    {
+        public int WorkerID { get; set; }
+        public int Salary { get; set; }
+        public double Discount { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
