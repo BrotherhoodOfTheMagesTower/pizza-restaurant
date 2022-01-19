cc199e796f5b2a2c1b7670db1a24b4c7d7e77279
Wojciech Janota <janota241@gmail.com>
2022-01-19 22:06:00 +0100

    Added crude Orders page without any interaction possible from the user

--- PizzaRestaurant/Controllers/HomeController.cs
+++ PizzaRestaurant/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace PizzaRestaurant.Controllers
             return View();
         }
 
+        public IActionResult Order()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
